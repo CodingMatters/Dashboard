975e91b9b956ea7c6a7643fcd32d4ad125ee2335
gabbydgab <gamba@gabbydgab.com>
2016-06-08 11:26:11 +0800

    initial commit

---
+++ config/dependencies.config.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * The Service Manager component can be configured by passing an associative array to the component's constructor.
+ *
+ * @see https://zendframework.github.io/zend-servicemanager/configuring-the-service-manager/
+ */
+
+use Dashboard\Factory;
+
+return [
+    'factories'          => [
+        "Application\Options\ModuleOptions"     => Factory\ModuleOptionsFactory::class,
+    ],
+    'services'           => [],
+    'abstract_factories' => [],
+    'delegators'         => [],
+    'shared'             => [],
+    'shared_by_default'  => true
+];
