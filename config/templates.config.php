a8b1e62b9def3e552cf5676ffbc24af43e704b05
gabbydgab <gamba@gabbydgab.com>
2016-06-10 13:48:22 +0800

    fixed implementation

--- config/templates.config.php
+++ config/templates.config.php
@@ -5,17 +5,17 @@ return [
     'view_manager' => [
         'layout'                => "dashboard/layout",
         'template_map'          => [
-            'dashboard/layout'  => __DIR__ . '/../view/layout/dashboard.phtml',
-            'template/footer'                   => __DIR__ . '/../view/layout/partial/footer.phtml',
-            'template/navigation/sidebar'       => __DIR__ . '/../view/layout/template/navigation/sidebar.phtml',
-            'template/navigation/header'        => __DIR__ . '/../view/layout/template/navigation/header.phtml',
-            'template/navigation/breadcrumbs'   => __DIR__ . '/../view/layout/template/navigation/breadcrumbs.phtml',
-            'partial/navigation/header'         => __DIR__ . '/../view/layout/partial/navigation/header.phtml',
-            'partial/navigation/sidebar'        => __DIR__ . '/../view/layout/partial/navigation/sidebar.phtml',
-            'partial/navigation/breadcrumbs'    => __DIR__ . '/../view/layout/partial/navigation/breadcrumbs.phtml',
+            'dashboard/layout'                  => __DIR__ . '/../templates/layout/dashboard.phtml',
+            'template/footer'                   => __DIR__ . '/../templates/layout/partial/footer.phtml',
+            'template/navigation/sidebar'       => __DIR__ . '/../templates/layout/template/navigation/sidebar.phtml',
+            'template/navigation/header'        => __DIR__ . '/../templates/layout/template/navigation/header.phtml',
+            'template/navigation/breadcrumbs'   => __DIR__ . '/../templates/layout/template/navigation/breadcrumbs.phtml',
+            'partial/navigation/header'         => __DIR__ . '/../templates/layout/partial/navigation/header.phtml',
+            'partial/navigation/sidebar'        => __DIR__ . '/../templates/layout/partial/navigation/sidebar.phtml',
+            'partial/navigation/breadcrumbs'    => __DIR__ . '/../templates/layout/partial/navigation/breadcrumbs.phtml',
         ],
         'template_path_stack'   => [
-            'dashboard' => __DIR__ . '/../view',
+            'dashboard' => __DIR__ . '/../templates',
         ],
     ],
 
@@ -23,17 +23,17 @@ return [
     'templates'     => [
         'layout'    => "dashboard/layout",
         'map'       => [
-            'dashboard/layout'  => __DIR__ . '/../view/layout/dashboard.phtml',
-            'template/footer'                   => __DIR__ . '/../view/layout/partial/footer.phtml',
-            'template/navigation/sidebar'       => __DIR__ . '/../view/layout/template/navigation/sidebar.phtml',
-            'template/navigation/header'        => __DIR__ . '/../view/layout/template/navigation/header.phtml',
-            'template/navigation/breadcrumbs'   => __DIR__ . '/../view/layout/template/navigation/breadcrumbs.phtml',
-            'partial/navigation/header'         => __DIR__ . '/../view/layout/partial/navigation/header.phtml',
-            'partial/navigation/sidebar'        => __DIR__ . '/../view/layout/partial/navigation/sidebar.phtml',
-            'partial/navigation/breadcrumbs'    => __DIR__ . '/../view/layout/partial/navigation/breadcrumbs.phtml',
+            'dashboard/layout'                  => __DIR__ . '/../templates/layout/dashboard.phtml',
+            'template/footer'                   => __DIR__ . '/../templates/layout/partial/footer.phtml',
+            'template/navigation/sidebar'       => __DIR__ . '/../templates/layout/template/navigation/sidebar.phtml',
+            'template/navigation/header'        => __DIR__ . '/../templates/layout/template/navigation/header.phtml',
+            'template/navigation/breadcrumbs'   => __DIR__ . '/../templates/layout/template/navigation/breadcrumbs.phtml',
+            'partial/navigation/header'         => __DIR__ . '/../templates/layout/partial/navigation/header.phtml',
+            'partial/navigation/sidebar'        => __DIR__ . '/../templates/layout/partial/navigation/sidebar.phtml',
+            'partial/navigation/breadcrumbs'    => __DIR__ . '/../templates/layout/partial/navigation/breadcrumbs.phtml',
         ],
         'paths'     => [
-            'dashboard' => [__DIR__ . '/../view']
+            'dashboard' => [__DIR__ . '/../templates']
         ]
     ],
 
