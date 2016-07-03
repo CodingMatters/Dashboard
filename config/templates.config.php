b0e8ec4b64f4bcd6478afb44864ccfb5d9af4dbb
gabbydgab <gamba@gabbydgab.com>
2016-07-04 07:07:22 +0800

    fixed template config

--- config/templates.config.php
+++ config/templates.config.php
@@ -1,9 +1,7 @@
 <?php
 
 return [
-    // Zend Framework Configuration
-    'view_manager' => [
-        'layout'                => "dashboard/layout",
+    'view_manager' => [        
         'template_map'          => [
             'dashboard/layout'                  => __DIR__ . '/../templates/layout/dashboard.phtml',            
             'template/navigation/sidebar'       => __DIR__ . '/../templates/layout/template/navigation/sidebar.phtml',
@@ -17,27 +15,6 @@ return [
             'dashboard' => __DIR__ . '/../templates',
         ],
     ],
-
-    // Zend Expressive Configuration
-    'templates'     => [
-        'layout'    => "dashboard/layout",
-        'map'       => [
-            'dashboard/layout'                  => __DIR__ . '/../templates/layout/dashboard.phtml',            
-            'template/navigation/sidebar'       => __DIR__ . '/../templates/layout/template/navigation/sidebar.phtml',
-            'template/navigation/header'        => __DIR__ . '/../templates/layout/template/navigation/header.phtml',
-            'template/navigation/breadcrumbs'   => __DIR__ . '/../templates/layout/template/navigation/breadcrumbs.phtml',
-            'partial/navigation/header'         => __DIR__ . '/../templates/layout/partial/navigation/header.phtml',
-            'partial/navigation/sidebar'        => __DIR__ . '/../templates/layout/partial/navigation/sidebar.phtml',
-            'partial/navigation/breadcrumbs'    => __DIR__ . '/../templates/layout/partial/navigation/breadcrumbs.phtml',
-        ],
-        'paths'     => [
-            'dashboard' => [__DIR__ . '/../templates']
-        ]
-    ],
-
-    /**
-     * Zend View Helpers
-     */
     'view_helpers'  => [
         'aliases'       => [],
         'invokables'    => [],
